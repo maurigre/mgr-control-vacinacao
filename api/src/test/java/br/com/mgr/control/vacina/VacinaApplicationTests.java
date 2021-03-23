ecb821827a46ca7f991bb5024f42710379d37765
Mauri <maurigre@gmail.com>
2021-03-23 13:30:46 -0300

    feat(init): estrutura inicial da api

---
+++ api/src/test/java/br/com/mgr/control/vacina/VacinaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mgr.control.vacina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
